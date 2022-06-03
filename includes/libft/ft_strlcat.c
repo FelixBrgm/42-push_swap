093c694dc8d3e336a72a5538deb80c1c9e1fa67c
SportStartup2022 <sport.startup.2022@gmail.com>
2022-06-03 23:18:01 +0200

    Working sort

--- includes/libft/ft_strlcat.c
+++ includes/libft/ft_strlcat.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fbruggem <fbruggem@student.42.fr>          +#+  +:+       +#+        */
+/*   By: felixbruggemann <felixbruggemann@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 13:50:24 by fbruggem          #+#    #+#             */
-/*   Updated: 2022/03/31 13:51:41 by fbruggem         ###   ########.fr       */
+/*   Updated: 2022/06/03 21:53:17 by felixbrugge      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,9 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	dstlen;
-	size_t	srclen;
 	size_t	ret;
 
 	dstlen = 0;
-	srclen = 0;
 	ret = ft_strlen(src) + ft_strlen(dst);
 	while (dstsize != 0 && *dst != '\0')
 	{
